7493a4dfed4d14bc99fd41c73e9ae71a3b86af10
zhengxinxin <zhengxinxin@b.360.cn>
2017-12-19 18:51:43 +0800

    all

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-// import Angular from 'angular';
-// import Router from '@uirouter/angularjs';
-// import ocLazyLoad from 'oclazyload';
-// import ngAnimate from 'angular-animate';
-// import ngVerify from 'ng-verify';
-// import ngFilter from 'angular-filter';
+import Angular from 'angular';
+import Router from '@uirouter/angularjs';
+import ocLazyLoad from 'oclazyload';
+import ngAnimate from 'angular-animate';
+import ngVerify from 'ng-verify';
+import ngFilter from 'angular-filter';
 
-// var BM = Angular.module("BM", [Router, ngAnimate, ngVerify, ngFilter]);
+const BM = Angular.module("BM", [Router, ngAnimate, ngVerify, ngFilter]);
+console.log(BM);
+BM.controller('bmCtrl', function($rootScope, $state, $scope, $interval, $filter, $location) {
 
-// BM.controller('bmCtrl', function($rootScope, $state, $scope, $interval, $filter, $location) {
 
-
-// })
+})
